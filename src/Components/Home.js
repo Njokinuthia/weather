9106522677cdea87e0e3e16e950dff0570ea46dc
Njoki <kinuthiash@gmail.com>
2022-05-24 22:25:27 +0300

    feat:add current time

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Time from "./Time"
+
+function Home(){
+
+  return(
+    <Time/>
+  )
+
+}
+
+export default Home;
--- src/Components/Time.js
+++ src/Components/Time.js
@@ -1,1 +1,29 @@
 
+function Time(){
+  // const [time ,setTime] = useState([])
+  const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+  const yearMonths = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"]
+ 
+  let today = new Date();
+  let day = today.getDay();
+  let date = today.getDate();
+  let month = today.getMonth();
+  let year = today.getFullYear();
+  let hour = today.getHours();
+  let minute = today.getMinutes();
+
+
+  console.log(`${weekDays[day]}  ${date} ${yearMonths[month]} ${year}`)
+
+  return(
+    <div>
+      <h3>{hour}:{minute} <span>hrs</span></h3>
+      <h3>{weekDays[day]} , {date} {yearMonths[month]} {year}</h3>
+
+    </div>
+  )
+
+}
+
+
+export default Time;
